32b82f9c7fa5dceca21848fd0a617e4aa1e1d9c8
limin <lmx@umich.edu>
2024-04-19 23:33:28 -0400

    push final ddpg code, and the model file of actor.

---
+++ test_model.py
@@ -0,0 +1,60 @@
+from env import rlmc_env
+from ddpg import *
+from utils import *
+
+
+# This file is used to test models
+# please change the parameters of env, "max_abs_action", "model_name", and parameters of DDPG
+if __name__ == "__main__":
+    env = rlmc_env("N-spring2D", 5, 0.005)  # Creat env
+    state_dim, action_dim = env.NNdims()
+    max_abs_action = 0.75
+    model_name = "N-spring2D_N=5_dt=0.005"
+    # Load torch model
+    model = torch.load("pth/" + model_name + ".pth")
+    agent = DDPG(state_dim, action_dim, max_abs_action, hidden_width0=256, hidden_width1=128, batch_size=256, lr=0.001,
+                 gamma=0.99, tau=0.005)
+    agent.actor = model
+    print("Simulation Start (from Actor)")
+    episodes = 100
+    steps = 300
+    scores = []
+    for episode in range(episodes):
+        env.reset_random(1.0)
+        state = env.get_current_state(n_dt=1)
+        score = 0
+        done = False
+        for step in range(steps):
+            action = agent.choose_action(state)
+            next_state, reward, _ = env.step(action, n_dt=1)
+            score += reward
+            state = next_state
+        scores.append(score)
+        if len(scores) < 10:
+            print("Episode {} average score: {}".format(episode, sum(scores) / len(scores)))
+        else:
+            print("Episode {} average score: {}".format(episode, sum(scores[-10:]) / 10))
+
+    # action from env
+    env = rlmc_env("N-spring2D", 5, 0.005)  # Creat env
+    state_dim, action_dim = env.NNdims()
+    print("Simulation Start (from env)")
+    episodes = 100
+    steps = 300
+    scores_env = []
+    for episode in range(episodes):
+        env.reset_random(1.0)
+        state = env.get_current_state(n_dt=1)
+        score = 0
+        for step in range(steps):
+            action = np.array(env.compute_forces(env.r)).flatten()
+            next_state, reward, _ = env.step(action, n_dt=1)
+            score += reward
+            state = next_state
+        scores_env.append(score)
+        if len(scores_env) < 10:
+            print("Episode {} average score: {}".format(episode, sum(scores) / len(scores)))
+        else:
+            print("Episode {} average score: {}".format(episode, sum(scores[-10:]) / 10))
+
+    plot2(scores, scores_env, 10, model_name)
